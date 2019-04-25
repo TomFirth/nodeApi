225874f359d587ac7aa60fb8b1c0be6dbf6716f9
Tom Firth <tom.firth@holidayextras.com>
2019-04-25 11:10:23 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 require('./endpoints')(app)
 
 const port = 12345
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json({
+  extended: true
+}))
 
 app.listen(port, () =>
   console.log(`Welcome to the HX Web Development API task - submission by Tom Firth\nListening on http://localhost:${port}`)
